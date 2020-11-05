fd51ac90283bf830efb1e33810b7f8c459c87412
JimNotCarry <59293465+JimNotCarry@users.noreply.github.com>
2020-11-05 09:22:14 +0100

    Subclass created

---
+++ src/Warrior.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linus
+ */
+public class Warrior extends Character {
+   
+    public Warrior(int initiativ, int tålighet, int attack, int smidighet) {
+        super(initiativ,  tålighet,  attack, smidighet);
+}
+    
+}
