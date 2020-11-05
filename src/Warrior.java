8bc18d42465e85d5a85e82d0654b34b8132d850d
JimNotCarry <59293465+JimNotCarry@users.noreply.github.com>
2020-11-05 13:19:13 +0100

    Update warrior

--- src/Warrior.java
+++ src/Warrior.java
@@ -2,16 +2,17 @@ public class Warrior extends Character {
     
     final String hero = "Warrior";
    
-    public Warrior(String name, int initiative, int endurance, int attack, int flexibility){
-           super(name,initiative,endurance,attack,flexibility);
+    public Warrior(String name, int initiativ, int tålighet, int attack, int smidighet) {
+        super(name,initiativ,  tålighet,  attack, smidighet);
     }
     
-     @Override
-    public String toString() {
-        return this.hero + "\nNamn: " + this.getName() + "\nInitiativ: " + this.getInitiative() + "\nTålighet: " + this.getEndurance() + "\nAttack: " + this.getAttack() + "\nSmidighet: " + this.getFlexibility();
-    }
     //Specialability();
     //FIRST impact per battle = blockchance 100%
     // from battle call method - hero.SpecialAbility(); - with body??
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
