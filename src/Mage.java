423df0d29fb9e2963c673fcf99b0af8cad3975dc
Salmihanna <73692594+Salmihanna@users.noreply.github.com>
2020-11-05 09:31:45 +0100

    hehe

---
+++ src/Mage.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sunni
+ */
+public class Mage extends Character{
+    public Mage(int initiativ, int tålighet, int attack, int smidighet) {
+        super(initiativ,  tålighet,  attack, smidighet);
+    }
+}
---
+++ src/Thief.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sunni
+ */
+public class Thief extends Character{
+
+    public Thief(int initiativ, int tålighet, int attack, int smidighet) {
+        super(initiativ, tålighet, attack, smidighet);
+    }
+    
+}
