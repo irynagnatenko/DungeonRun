8a70b40df238b8f5045283aa1b058a0e9695626d
soderkisen <73692272+soderkisen@users.noreply.github.com>
2020-11-13 13:25:27 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 package src;
 import java.util.concurrent.ThreadLocalRandom;
-// ny till github 3
+// ny till github 4
 /**
  * Created by Iryna Gnatenko Date 10/27/2020 Time 1:32 PM Project untitled1
  */
